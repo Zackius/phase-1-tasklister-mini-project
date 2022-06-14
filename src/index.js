53044210ec95725a9f7b59d5c52495cc7bb65dff
Zacharia <ndunguzachary24@gmail.com>
2022-06-14 15:49:27 +0300

    my todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ let  form = document.querySelector('form').addEventListener('submit',  (e) =>{
+  e.preventDefault();
+  creatingTodoApp(e.target.new_task.value)
+ form.reset()
+ })
+
+ })
+ 
+function creatingTodoApp(main){
+   let li = document.createElement('li')
+   let button = document.createElement('button')
+  button.addEventListener('click',  deleteItems)
+  button.textContent = 'Delete Item'
+  li.textContent =`${main}`
+  li.appendChild(button)
+
+document.querySelector('#list').appendChild(li)
+}
+function deleteItems(e){
+  e.target.parentNode.remove()
+}
